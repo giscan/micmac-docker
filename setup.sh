c139ba1c53a83bd4f7dcc21f1b504eeef2500a55
Sylvain POULAIN <kikislater@users.noreply.github.com>
2016-06-02 13:41:57 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-cd 
 echo "*******************************************************"
 echo "*********       INSTALLING LIBRAIRIES         *********"
 echo "*******************************************************"
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install x11proto-core-dev make cmake libx11-dev mercurial subversion imagemagick gcc g++ exiv2 libimage-exiftool-perl libgeo-proj4-perl mesa-common-dev libgl1-mesa-dev libglapi-mesa libglu1-mesa qt5-default doxygen opencl-headers
+apt-get update && apt-get install x11proto-core-dev make \
+cmake libx11-dev mercurial subversion imagemagick gcc g++ \
+exiv2 libimage-exiftool-perl libgeo-proj4-perl \ 
+mesa-common-dev libgl1-mesa-dev libglapi-mesa libglu1-mesa \
+qt5-default doxygen opencl-headers
 echo "*******************************************************"
 echo "**********       LIBRAIRIES INSTALLED        **********"
 echo "*******************************************************"
@@ -26,13 +27,13 @@ echo " "
 echo " "
 echo " "
 echo "*******************************************************"
-echo "********          COMPILING SOFTWARE          *********"
+echo "********          BUILDING  SOFTWARE          *********"
 echo "*******************************************************"
 cd micmac
 mkdir build
 cd build
 cmake -DBUILD_PATH_BIN="/usr/bin" \
-	-DDEPLOY=1
+	-DDEPLOY=1 \
 	-DWITH_QT5=1 \
 	-DBUILD_POISSON=1 \
 	-DBUILD_RNX2RTKP=1 \
@@ -40,18 +41,16 @@ cmake -DBUILD_PATH_BIN="/usr/bin" \
 	-DWITH_OPEN_MP=ON \
 	-DWITH_ETALONPOLY=ON \
 	-DWITH_DOXYGEN=ON \
-	-DBUILD_POISSON=ON  ../
+	../
 NBRP=$(cat /proc/cpuinfo | grep processor | wc -l)
 make -j$NBRP
 make install
 echo "*******************************************************"
-echo "************       SOFTWARE COMPILED       ************"
+echo "************       SOFTWARE BUILDING       ************"
 echo "*******************************************************"
 echo "*******************************************************"
-echo "\nexport PATH=$PATH:~/culture3d/bin" >> ~/.bashrc
 echo "*******************************************************"
 echo "********    MicMac should now be set up     **********"
-echo "********    in your personal repository     **********"
 echo "********  Please report any inconvenience   **********"
 echo "******** http://forum-micmac.forumprod.com  **********"
 echo "******** Edit by Sylvain POULAIN for Docker **********"
